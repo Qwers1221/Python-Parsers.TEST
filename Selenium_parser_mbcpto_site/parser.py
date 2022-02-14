5ad61e36a6dbb1395adba0b648081adddd4d1586
Qwers1221 <stialerminers@gmail.com>
2022-02-14 15:56:56 +0200

    add comments

--- Selenium_parser_mbcpto_site/parser.py
+++ Selenium_parser_mbcpto_site/parser.py
@@ -5,12 +5,15 @@ from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from auth_data import *
 
+# Функция авторизации в Instagram
 def login(username, password):
+    # Подключение WebDriver(Chrome) через менеджер, обычный способ из директории не сработал!
     browser = webdriver.Chrome(ChromeDriverManager().install())
-    # executable_path=r'C:\\Users\\Admin\\Downloads\\chromedriver\\chromedriver.exe'
+
     browser.get("https://www.instagram.com/")
     time.sleep(random.randrange(3, 5))
 
+    # Закрытие окна, и на всякий случай браузер полностью
     browser.close()
     browser.quit()
 
