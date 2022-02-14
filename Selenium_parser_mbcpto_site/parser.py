898b27a37e7bb87ba25100dc5fea5b9ad74a3bac
Qwers1221 <stialerminers@gmail.com>
2022-02-14 15:44:25 +0200

    upg chromedriver

--- Selenium_parser_mbcpto_site/parser.py
+++ Selenium_parser_mbcpto_site/parser.py
@@ -2,11 +2,12 @@ import time, random
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 from auth_data import *
 
 def login(username, password):
-    browser = webdriver.Chrome(executable_path=r'chromedriver.exe')
-
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    # executable_path=r'C:\\Users\\Admin\\Downloads\\chromedriver\\chromedriver.exe'
     browser.get("https://www.instagram.com/")
     time.sleep(random.randrange(3, 5))
 
