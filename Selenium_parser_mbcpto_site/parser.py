14a4614b6a67248561733747cced268337379e66
Qwers1221 <stialerminers@gmail.com>
2022-02-14 16:13:46 +0200

    parser upg

--- Selenium_parser_mbcpto_site/parser.py
+++ Selenium_parser_mbcpto_site/parser.py
@@ -13,6 +13,20 @@ def login(username, password):
     browser.get("https://www.instagram.com/")
     time.sleep(random.randrange(3, 5))
 
+    username_input = browser.find_elements_by_name("username")
+    username_input.clear()
+    username_input.send_keys(username)
+
+    time.sleep(2)
+
+    password_input = browser.find_elements_by_name("password")
+    password_input.clear()
+    password_input.send_keys(password)
+
+    time.sleep(2)
+
+    password_input.send_keys(Keys.ENTER)
+
     # Закрытие окна, и на всякий случай браузер полностью
     browser.close()
     browser.quit()
