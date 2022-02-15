6c0d9efa889b503bc6481a85f58ea811d505cc30
Qwers1221 <stialerminers@gmail.com>
2022-02-15 10:10:01 +0200

    add comments

--- Selenium_parser_mbcpto_site/parser.py
+++ Selenium_parser_mbcpto_site/parser.py
@@ -12,21 +12,18 @@ def login(username, password):
     # Загрузка сайта + ждем от 3 до 5 сек для уверености
     browser.get("https://www.instagram.com/")
     time.sleep(random.randrange(3, 5))
-    # Ищем елемент username на странице, очищаем + заполняем именем из файла auth_data
+    # Ищем елемент username на странице, очищаем + заполняем именем из файла auth_data, ждем 2 сек для уверености
     username_input = browser.find_element_by_name("username")
     username_input.clear()
     username_input.send_keys(username)
-
     time.sleep(2)
-
+    # Ищем елемент password на странице, очищаем + заполняем паролем из файла auth_data, ждем 2 сек для уверености
     password_input = browser.find_element_by_name("password")
     password_input.clear()
     password_input.send_keys(password)
-
     time.sleep(2)
-
+    # Нажимаем клавишу Enter в поле пароля + ждем 2 сек для уверености
     password_input.send_keys(Keys.ENTER)
-
     time.sleep(2)
 
     # Закрытие окна, и на всякий случай браузер полностью
